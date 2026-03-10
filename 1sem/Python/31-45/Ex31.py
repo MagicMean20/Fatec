427e332adfd77d596883a9811607a7a6b9143b94
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-10 09:28:05 -0300

    Inicio Exs. 31-45

---
+++ 1sem/Python/31-45/Ex31.py
@@ -0,0 +1,4 @@
+n = 10
+while n <= 150:
+    print(n*n,": Quadrado de",n)
+    n += 1
---
+++ 1sem/Python/31-45/Ex32.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número: "))
+t = 1
+
+while a>1:
+    t = t * a
+    a = a - 1
+
+print("O fatorial é: ", t)
