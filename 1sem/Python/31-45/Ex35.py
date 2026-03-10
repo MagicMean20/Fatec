3feb3314519cdb635835a5c09a0cc1155c929896
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-10 09:50:55 -0300

    Ex35

---
+++ 1sem/Python/31-45/Ex35.py
@@ -0,0 +1,26 @@
+a = int(input("Digite um valor: "))
+b = int(input("Digite outro valor: "))
+soma = 0
+
+if a < b:
+    # caso a seja menor que b
+    if a % 2 == 0:
+        a += 1
+    while soma < b:
+        soma += a
+        a += 2
+
+        # para evitar que o programa imprima um valor maior que b
+        if soma < b:
+            print(soma)
+else:
+    # caso b seja menor que a
+    if b % 2 == 0:
+        b += 1
+    while soma < a:
+        soma += b
+        b += 2
+
+        # para evitar que o programa imprima um valor maior que a
+        if soma < a:
+            print(soma)
