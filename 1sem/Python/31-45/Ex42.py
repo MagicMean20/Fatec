a39804e8d943c9c22ee1f37dedfd45d2c423727e
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-12 07:48:38 -0300

    Ex43

---
+++ 1sem/Python/31-45/Ex42.py
@@ -0,0 +1,2 @@
+# Calcule e mostre a série 1 + 2/3 + 3/5 + ... + 50/99
+soma = 1
---
+++ 1sem/Python/31-45/Ex43.py
@@ -0,0 +1,10 @@
+ana = 1.1
+maria = 1.5
+cont = 0
+
+while ana < maria:
+    ana += 0.3
+    maria += 0.2
+    cont += 1
+    
+print("Ana ultrapassará Maria em", cont, "anos.")    
