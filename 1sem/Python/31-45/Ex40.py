d2f468247a9248749656303df6cc8dcbc5339652
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-12 07:38:57 -0300

    Ex40

--- 1sem/Python/31-45/Ex40.py
+++ 1sem/Python/31-45/Ex40.py
@@ -7,14 +7,11 @@ if n1 > n2:
     n1 = n2
     n2 = c
 
-if n1 > 1 or n2 > 1:
-    print("Números primos entre", n1, "e", n2, ":")
-    for num in range(n1, n2 + 1):
-        if num > 1:
-            for i in range(2, int(num**0.5) + 1):
-                if num % i == 0:
-                    break
-            else:
-                print(num)
-else:
-    print("Digite números maiores que 1.")
+print("Números primos entre", n1, "e", n2, ":")
+for num in range(n1, n2 + 1):
+    if num > 1:
+        for i in range(2, int(num/2) + 1):
+            if num % i == 0:
+                break
+        else:
+            print(num, end=" ")
