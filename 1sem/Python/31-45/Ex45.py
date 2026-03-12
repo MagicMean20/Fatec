d25cd302e57b79f077498f0f8a8c9d24b16bda30
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-12 09:15:08 -0300

    Ex45

---
+++ 1sem/Python/31-45/Ex45.py
@@ -0,0 +1,14 @@
+
+sub = 1
+c = 2
+
+while c < 15:
+    sub = sub + c / (c*c)
+    c = c + 1
+    print(c, '+' ,sub)
+    if c < 15:
+        sub = sub - c / (c*c)
+        c = c + 1
+        print(c, '-' ,sub)
+
+print("O valor da série é: {:.2f}".format(sub))
