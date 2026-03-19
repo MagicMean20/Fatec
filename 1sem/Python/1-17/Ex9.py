5935924d4dc9e732b4c63beee33b218cbb821621
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:50:44 -0300

    Mod Ex9

--- 1sem/Python/1-17/Ex9.py
+++ 1sem/Python/1-17/Ex9.py
@@ -4,9 +4,9 @@ while n1 <= 0 or n2 <= 0:
     n1 = int(input("Digite um número inteiro: "))
     n2 = int(input("Digite outro número inteiro: "))
 
-q1 = n1*n1
-q2 = n2*n2
+def soma():
+    q1 = n1*n1
+    q2 = n2*n2
+    return q1 + q2
 
-soma = q1 + q2
-
-print("A soma dos quadrados deles é: ", soma)
+print("A soma dos quadrados deles é: ", soma())
