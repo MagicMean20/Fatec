b8ef93967562dd0aec8fac276e7d57f5065f608b
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:07:44 -0300

    Mod Ex18

--- 1sem/Python/18-29/Ex18.py
+++ 1sem/Python/18-29/Ex18.py
@@ -4,11 +4,12 @@ while n1 <= 0 or n2 <= 0:
     n1 = int(input("Digite o primeiro número: "))
     n2 = int(input("Digite o segundo número: "))
 
-dif = n1 - n2
+def diferenca():
+    dif = n1 - n2
 
-if dif < 0:
-    print("O segundo número ({}) é maior que o primeiro ({}).".format(n2, n1))
-elif dif > 0:
-    print("O primeiro número ({}) é maior que o segundo ({}).".format(n1, n2))
-else:
-    print("Os números são iguais.")
+    if dif < 0:
+        return "O segundo número ({}) é maior que o primeiro ({}).".format(n2, n1)
+    elif dif > 0:
+        return "O primeiro número ({}) é maior que o segundo ({}).".format(n1, n2)
+    else:
+        return "Os números são iguais."
