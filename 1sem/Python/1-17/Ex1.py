5a2386f70ea7e9537cbeffea10ac9b229ebe8839
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:24:55 -0300

    Mod Ex1

--- 1sem/Python/1-17/Ex1.py
+++ 1sem/Python/1-17/Ex1.py
@@ -1,4 +1,6 @@
+def calcular_area_quadrado():
+    return lado * lado
 lado=0
 while lado <= 0:
     lado = int(input("Digite o valor do lado do quadrado: "))
-print(f"A área do quadrado é: {lado ** 2}")
+print(f"A área do quadrado é: {calcular_area_quadrado()}")
