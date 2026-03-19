83e702f1995086d79f958992a930e9ae7b058eda
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:25:40 -0300

    Mod Ex2

--- 1sem/Python/1-17/Ex2.py
+++ 1sem/Python/1-17/Ex2.py
@@ -1,5 +1,6 @@
+def salario():
+    return sal * 1.15
 sal=0
 while sal <= 0:
     sal = float(input("Digite o salário do funcionário: "))
-liq = sal *1.15
-print("O novo salário do funcionário é: R$ {:.2f}".format(liq))
+print("O novo salário do funcionário é: R$ {:.2f}".format(salario()))
