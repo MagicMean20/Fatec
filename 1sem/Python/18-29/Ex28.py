33f7da9e802fc467f34609abe0b11f9f4c91c6dc
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:33:15 -0300

    Mod Ex28

--- 1sem/Python/18-29/Ex28.py
+++ 1sem/Python/18-29/Ex28.py
@@ -10,16 +10,19 @@ while precoA <= 0 or med <= 0:
 # >=1000	  >=80	       -         5%
 # Em outras condições, o preço do produto não sofre ajuste.
 
-if med < 500 and precoA < 30:
-    taxa = 0.10
-    precoA += precoA * taxa
-elif 500 <= med < 1000 and 30 <= precoA < 80:
-    taxa = 0.15
-    precoA += precoA * taxa
-elif med >= 1000 and precoA >= 80:
-    taxa = 0.05
-    precoA -= precoA * taxa
-else:
-    taxa = 0
+def alteracao():
+    if med < 500 and precoA < 30:
+        taxa = 0.10
+        precoA += precoA * taxa
+    elif 500 <= med < 1000 and 30 <= precoA < 80:
+        taxa = 0.15
+        precoA += precoA * taxa
+    elif med >= 1000 and precoA >= 80:
+        taxa = 0.05
+        precoA -= precoA * taxa
+    else:
+        taxa = 0
 
-print(f"Preço ajustado: R${precoA:.2f}")
+    return f"Preço ajustado: R${precoA:.2f}"
+
+print(alteracao())
