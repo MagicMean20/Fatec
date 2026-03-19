1fffd5d1648f3cd74082893b0d7c3576a3fcc627
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:28:32 -0300

    Mod Ex4

--- 1sem/Python/1-17/Ex4.py
+++ 1sem/Python/1-17/Ex4.py
@@ -1,5 +1,8 @@
+def conversao():
+    fah = (9*cel+160) /5
+    return fah
+
 cel=0
 while cel <= 0:
     cel = int(input("Digite a temperatura em Celsius: "))
-fah = (9*cel+160) /5
-print("A temperatura em Fahrenheit é:", fah)
+print("A temperatura em Fahrenheit é:", conversao())
