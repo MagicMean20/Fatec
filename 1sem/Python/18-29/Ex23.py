8b97db9c6af419c2a2878c9104985a7f152cbf1a
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:17:50 -0300

    Mod Ex23

--- 1sem/Python/18-29/Ex23.py
+++ 1sem/Python/18-29/Ex23.py
@@ -8,11 +8,14 @@ while a <= 0 or b <= 0 or c <= 0 or d <= 0:
     c = int(input("Digite um número ainda maior: "))
     d = int(input("Insira qualquer número agora:"))
 
-if d < a:
-    print(f"A ordem é: {d}, {a}, {b}, {c}")
-elif d < b:
-    print(f"A ordem é: {a}, {d}, {b}, {c}") 
-elif d < c:
-    print(f"A ordem é: {a}, {b}, {d}, {c}") 
-else:
-    print(f"A ordem é: {a}, {b}, {c}, {d}")
+def ordem():
+    if d < a:
+        return f"A ordem é: {d}, {a}, {b}, {c}"
+    elif d < b:
+        return f"A ordem é: {a}, {d}, {b}, {c}"
+    elif d < c:
+        return f"A ordem é: {a}, {b}, {d}, {c}"
+    else:
+        return f"A ordem é: {a}, {b}, {c}, {d}"
+    
+print(ordem())
