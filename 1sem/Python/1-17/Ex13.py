aacecdfc70db7cf5d558eec0da3793ff297ccdd8
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:55:06 -0300

    Mod Ex13

--- 1sem/Python/1-17/Ex13.py
+++ 1sem/Python/1-17/Ex13.py
@@ -2,6 +2,7 @@ quilos=0
 while quilos <= 0:
     quilos = float(input("Digite a quantidade de quilos de alimento: "))
 
-dias = int(quilos / 0.05)
+def dias():
+    return int(quilos / 0.05)
 
-print(f"O alimento durará {dias} dias.")
+print(f"O alimento durará {dias()} dias.")
