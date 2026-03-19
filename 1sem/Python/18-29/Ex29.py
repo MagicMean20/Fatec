d8ee465fe0f865a1778af18b6cb9135ac90ff211
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:34:11 -0300

    Mod Ex29

--- 1sem/Python/18-29/Ex29.py
+++ 1sem/Python/18-29/Ex29.py
@@ -4,14 +4,17 @@ while tipo == 0 or tipo > 2:
              "1 - Poupança\n" \
              "2 - Renda Fixa\n" \
              "Digite o tipo desejado: "))
-    
+
 if tipo == 1:
     taxa = 0.03
 else:
     taxa = 0.05
     
 valor = float(input("Valor a ser investido: "))
-    
-valorF = valor + (valor * taxa)
-print(f"Valor final do investimento: R${valorF:.2f}")
+
+def calcular_investimento():
+    valorF = valor + (valor * taxa)
+    return f"Valor final do investimento: R${valorF:.2f}"
+
+print(calcular_investimento())
 
