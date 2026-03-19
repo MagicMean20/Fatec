e613a60f0370f880c5067e33c754d96fbb788686
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:52:51 -0300

    Mod Ex10

--- 1sem/Python/1-17/Ex10.py
+++ 1sem/Python/1-17/Ex10.py
@@ -4,11 +4,11 @@ while r1 <= 0 or r2 <= 0:
     r1 = float(input("Digite um número: "))
     r2 = float(input("Digite outro número: "))
 
-d = r1 - r2
-
-if d < 0:
-    print("A diferença é de {:.2f}.".format(d*(-1)))
-elif d == 0:
-    print("A diferença é 0")
-else:
-    print("A diferença é de {:.2f}.".format(d))
+def diferenca():
+    d = r1 - r2
+    if d < 0:
+        return f"A diferença é de {d*(-1):.2f}."
+    elif d == 0:
+        return("A diferença é 0")
+    else:
+        return f"A diferença é de {d:.2f}."
