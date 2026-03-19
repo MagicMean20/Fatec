574f1245f09787e1d3802d39b2b88a3069afc03c
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:34:20 -0300

    Mod Ex5

--- 1sem/Python/1-17/Ex5.py
+++ 1sem/Python/1-17/Ex5.py
@@ -1,3 +1,16 @@
+def raizes():
+    # achando o delta
+    delta = b**2 - 4*a*c
+    raiz1 = (-b + delta**0.5) / (2*a)
+    raiz2 = (-b - delta**0.5) / (2*a)
+    if delta >= 0:
+        return "A equação tem raízes reais."
+    elif delta == 0:
+        return "A equação tem uma raiz real:", raiz1
+    else:
+        return "A equação tem duas raízes reais.", raiz1, "e", raiz2
+
+
 a=0
 b=0
 c=0
@@ -5,11 +18,4 @@ while a <= 0 or b <= 0 or c <= 0:
     a = int(input("Digite um número inteiro: "))
     b = int(input("Digite outro número inteiro: "))
     c = int(input("Digite mais um número inteiro: "))
-
-# achando o delta
-delta = b**2 - 4*a*c
-
-raiz1 = (-b + delta**0.5) / (2*a)
-raiz2 = (-b - delta**0.5) / (2*a)
-
-print("As raízes da equação são:", raiz1, "e", raiz2)
+print(raizes())
