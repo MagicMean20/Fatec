af6ec111d695bf456ef7d1e1c1bdc60babd53af1
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:37:13 -0300

    Mod Ex8

--- 1sem/Python/1-17/Ex8.py
+++ 1sem/Python/1-17/Ex8.py
@@ -1,7 +1,10 @@
+def rendimento():
+    novoVal = valor * 1.3
+    return novoVal
+
 valor=0
 while valor <=0:
     valor = float(input("Digite o depósito desejado: "))
 
-novoVal = valor *1.3
 
-print("O valor do depósito com o rendimento é: R$ %.2f" %novoVal)
+print("O valor do depósito com o rendimento é: R$ %.2f" %rendimento())
