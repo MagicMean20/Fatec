f06ff05c6de969b4c0dcfe6e7cc731212ed98da5
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:00:59 -0300

    Mod Ex16

--- 1sem/Python/1-17/Ex15.py
+++ 1sem/Python/1-17/Ex15.py
@@ -4,5 +4,7 @@ while cat1 <= 0 or cat2 <= 0:
     cat1 = float(input("Digite a medida do cateto oposto: "))
     cat2 = float(input("Digite a medida do cateto adjacente: "))
 
-hip = (cat1 ** 2 + cat2 ** 2) ** 0.5
-print(f"A medida da hipotenusa é: {hip}")
+def hipot():
+    return (cat1 ** 2 + cat2 ** 2) ** 0.5
+
+print(f"A medida da hipotenusa é: {hipot()}")
