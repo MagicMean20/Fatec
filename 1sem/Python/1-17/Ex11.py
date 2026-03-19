55ae59c1de416c97876449ce08cedf3f54d12e3c
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:53:23 -0300

    Mod Ex11

--- 1sem/Python/1-17/Ex11.py
+++ 1sem/Python/1-17/Ex11.py
@@ -2,6 +2,7 @@ raio=0
 while raio <=0:
     raio = float(input("Digite o raio do círculo: "))
 
-comp = 2 * 3.14 * raio
+def comp():
+    return 2 * 3.14 * raio
 
 print("O comprimento desse círculo é:", comp)
