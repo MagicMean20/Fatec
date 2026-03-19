bb208d3c7672c43bb2c4f8803b4e443739cf5176
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 10:27:32 -0300

    Mod Ex3

--- 1sem/Python/1-17/Ex3.py
+++ 1sem/Python/1-17/Ex3.py
@@ -1,7 +1,8 @@
+def area():
+    return (base*altura)/2
 base=0
 altura=0
 while base <=0 or altura <=0:
     base = int(input("Digite a base do triângulo: "))
     altura = int(input("Digite a altura do triângulo: "))
-area = (base * altura) / 2
-print("A área do triângulo é:", area)
+print("A área do triângulo é:", area())
