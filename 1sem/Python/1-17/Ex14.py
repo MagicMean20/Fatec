1403bcc57308ac9f752dd95710632ec0183fd80c
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:00:22 -0300

    Mod Ex14

--- 1sem/Python/1-17/Ex14.py
+++ 1sem/Python/1-17/Ex14.py
@@ -4,6 +4,8 @@ while ang1 <=0 or ang2 <= 0:
     ang1 = int(input("Digite o primeiro ângulo do triângulo: "))
     ang2 = int(input("Digite o segundo ângulo do triângulo: "))
 
-ang3 = 180 - (ang1 + ang2)
+def ang():
+    ang3 = 180 - (ang1 + ang2)
+    return ang3
 
-print(f"O terceiro ângulo do triângulo é: {ang3} graus.")
+print(f"O terceiro ângulo do triângulo é: {ang()} graus.")
