f02e509cbc67d184313bbc160a9369899240c411
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:30:12 -0300

    Mod Ex26

--- 1sem/Python/18-29/Ex26.py
+++ 1sem/Python/18-29/Ex26.py
@@ -1,17 +1,18 @@
 n1 = int(input("Informe um número: "))
 n2 = int(input("Dê outro número: "))
 
-if n1 > n2:
-    som = n1 % n2
-    if som == 0:
-        print(f"{n1} é maior que {n2} e também é divisível")
+def maioredivisivel():
+    if n1 > n2:
+        som = n1 % n2
+        if som == 0:
+            return f"{n1} é maior que {n2} e também é divisível"
+        else:
+            return f"{n1} é maior que {n2}"
+    elif n2 > n1:
+        som = n2 % n1
+        if som == 0:
+            return f"{n2} é maior que {n1} e também é divisível"
+        else:
+            return f"{n2} é maior que {n1}"
     else:
-        print(f"{n1} é maior que {n2}")
-elif n2 > n1:
-    som = n2 % n1
-    if som == 0:
-        print(f"{n2} é maior que {n1} e também é divisível")
-    else:
-        print(f"{n2} é maior que {n1}")
-else:
-    print("Números iguais")
+        return "Números iguais"
