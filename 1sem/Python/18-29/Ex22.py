c6508ffa526e2312c49a1b56ca0a6878a699eda0
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-19 11:17:19 -0300

    Mod Ex22

--- 1sem/Python/18-29/Ex22.py
+++ 1sem/Python/18-29/Ex22.py
@@ -4,9 +4,12 @@ while v1 <= 0 or v2 <= 0:
     v1 = int(input("Digite o primeiro valor: "))
     v2 = int(input("Digite o segundo valor: "))
 
-if v1 > v2:
-    print(f"A ordem crescente dos valores é: {v2}, {v1}")
-elif v2 > v1:
-    print(f"A ordem crescente dos valores é: {v1}, {v2}")
-else:
-    print("Valores iguais")
+def ordem():
+    if v1 > v2:
+        return f"A ordem crescente dos valores é: {v2}, {v1}"
+    elif v2 > v1:
+        return f"A ordem crescente dos valores é: {v1}, {v2}"
+    else:
+        return "Valores iguais"
+    
+print(ordem())
