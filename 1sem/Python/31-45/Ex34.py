d3d513b458fe450686b8f5edc4081ffd7527c225
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-20 09:26:37 -0300

    Mod Ex 34

--- 1sem/Python/31-45/Ex34.py
+++ 1sem/Python/31-45/Ex34.py
@@ -1,8 +1,13 @@
+global d
 n=0
+d = 1
 while n <= 0:
     n = int(input("Digite um número inteiro: "))
-d = 1
 
-while d <= 12:
-    print(n*d, f": {n} x {d}")
-    d += 1
+def tabuada(numero):
+    global d
+    while d <= 12:
+        print(numero*d, f": {numero} x {d}")
+        d += 1
+
+tabuada(n)
