0d7d671f9149716429f4aa474412748938931a63
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-21 11:40:14 -0300

    Main 23

--- 1sem/Python/18-29/Ex23.py
+++ 1sem/Python/18-29/Ex23.py
@@ -1,21 +1,27 @@
-a=0
-b=0
-c=0
-d=0
-while a <= 0 or b <= 0 or c <= 0 or d <= 0:
-    a = int(input("Digite um número pequeno: "))
-    b = int(input("Digite um número maior que o anterior: "))
-    c = int(input("Digite um número ainda maior: "))
-    d = int(input("Insira qualquer número agora:"))
+global a,b,c,d
+def __main__():
+    global a,b,c,d
+    a=0
+    b=0
+    c=0
+    d=0
+    while a <= 0 or b <= 0 or c <= 0 or d <= 0:
+        a = int(input("Digite um número pequeno: "))
+        b = int(input("Digite um número maior que o anterior: "))
+        c = int(input("Digite um número ainda maior: "))
+        d = int(input("Insira qualquer número agora:"))
+    ordem()
 
 def ordem():
+    global a,b,c,d
     if d < a:
-        return f"A ordem é: {d}, {a}, {b}, {c}"
+        print(f"A ordem é: {d}, {a}, {b}, {c}")
     elif d < b:
-        return f"A ordem é: {a}, {d}, {b}, {c}"
+        print(f"A ordem é: {a}, {d}, {b}, {c}")
     elif d < c:
-        return f"A ordem é: {a}, {b}, {d}, {c}"
+        print(f"A ordem é: {a}, {b}, {d}, {c}")
     else:
-        return f"A ordem é: {a}, {b}, {c}, {d}"
+        print(f"A ordem é: {a}, {b}, {c}, {d}")
     
-print(ordem())
+if "__name__" == __main__():
+    ordem()
