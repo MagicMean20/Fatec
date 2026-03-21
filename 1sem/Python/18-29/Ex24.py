d3af8f23bda1c14f8d931f0fcba2e5dd276b950e
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-21 16:03:34 -0300

    Main 24

--- 1sem/Python/18-29/Ex24.py
+++ 1sem/Python/18-29/Ex24.py
@@ -1,11 +1,17 @@
-val=0
-while val <= 0:
-    val = int(input("Digite um número:"))
+global val
+def __main__():
+    global val
+    val=0
+    while val <= 0:
+        val = int(input("Digite um número:"))
+    divisivel()
 
 def divisivel():
+    global val
     if val % 2 == 0 and val % 3 == 0:
         return "O valor é divisível por 2 e 3"
     else:
         return "O valor não é divisível por 2 e 3"
     
-print(divisivel())
+if "__name__" == __main__():
+    divisivel()
