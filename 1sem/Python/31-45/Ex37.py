721c9302f50a76adddab909ae3f9e3735bac8261
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-21 11:07:59 -0300

    Mod Ex 37

--- 1sem/Python/31-45/Ex37.py
+++ 1sem/Python/31-45/Ex37.py
@@ -4,18 +4,19 @@ while N <= 0:
 N-=3
 A = 1
 B = 1
-F = 1
-print("0")
-print(A)
-print(B)
-while N != 0:
-    C = A+B
-    print(C)
-    N-=1
-    if F%2==0:
-        A=C
-        F+=1
-    else:
-        B=C
-        F+=1
-        # TODO: write code...
+F = 2
+print('1')
+print('1')
+
+def fibo(pri, seg, res, cont):
+    while cont != 0:
+        print(pri + seg)
+        if res%2==0:
+            pri = res
+            res+=seg
+        else:
+            seg = res
+            res+=pri
+        cont -= 1
+
+fibo(A, B, F, N)
