1aced924ceb710af0a0b0686af27ce58a8775b81
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-21 11:34:45 -0300

    Main 21

--- 1sem/Python/18-29/Ex21.py
+++ 1sem/Python/18-29/Ex21.py
@@ -1,23 +1,29 @@
-nota1=0
-nota2=0
-nota3=0
-nota4=0
-while nota1 <= 0 or nota2 <= 0 or nota3 <= 0 or nota4 <= 0:
-    nota1 = float(input("Digite a nota do primeiro bimestre: "))
-    nota2 = float(input("Digite a nota do segundo bimestre: "))
-    nota3 = float(input("Digite a nota do terceiro bimestre: "))
-    nota4 = float(input("Digite a nota do quarto bimestre: "))
+global nota1,nota2,nota3,nota4
+def __main__():
+    global nota1,nota2,nota3,nota4
+    nota1=0
+    nota2=0
+    nota3=0
+    nota4=0
+    while nota1 <= 0 or nota2 <= 0 or nota3 <= 0 or nota4 <= 0:
+        nota1 = float(input("Digite a nota do primeiro bimestre: "))
+        nota2 = float(input("Digite a nota do segundo bimestre: "))
+        nota3 = float(input("Digite a nota do terceiro bimestre: "))
+        nota4 = float(input("Digite a nota do quarto bimestre: "))
+    media()
 
 def media():
+    global nota1,nota2,nota3,nota4
     media = (nota1 + nota2 + nota3 + nota4) / 4
 
     if media < 3:
-        return "RETIDO"
+        print("RETIDO")
 
     elif media >= 3 and media < 6:
-        return "EXAME"
+        print("EXAME")
 
     else:
-        return "APROVADO"
+        print("APROVADO")
     
-print(media())
+if "__name__" == __main__():
+    media()
--- 1sem/Python/18-29/Ex26.py
+++ 1sem/Python/18-29/Ex26.py
@@ -1,20 +1,26 @@
-n1 = int(input("Informe um número: "))
-n2 = int(input("Dê outro número: "))
+global n1,n2
+def __main__():
+    global n1,n2
+    n1 = int(input("Informe um número: "))
+    n2 = int(input("Dê outro número: "))
+    maioredivisivel()
 
 def maioredivisivel():
+    global n1,n2
     if n1 > n2:
         som = n1 % n2
         if som == 0:
-            return f"{n1} é maior que {n2} e também é divisível"
+            print(f"{n1} é maior que {n2} e também é divisível")
         else:
-            return f"{n1} é maior que {n2}"
+            print(f"{n1} é maior que {n2}")
     elif n2 > n1:
         som = n2 % n1
         if som == 0:
-            return f"{n2} é maior que {n1} e também é divisível"
+            print(f"{n2} é maior que {n1} e também é divisível")
         else:
-            return f"{n2} é maior que {n1}"
+            print(f"{n2} é maior que {n1}")
     else:
-        return "Números iguais"
+        print("Números iguais")
     
-print(maioredivisivel())
+if "__name__" == __main__():
+    maioredivisivel()
