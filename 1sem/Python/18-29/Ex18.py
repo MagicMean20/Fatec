f5e16cb3de3ef29cdc64da75151068e64b7d0076
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-21 11:20:50 -0300

    Main 18

--- 1sem/Python/18-29/Ex18.py
+++ 1sem/Python/18-29/Ex18.py
@@ -1,17 +1,24 @@
-n1=0
-n2=0
-while n1 <= 0 or n2 <= 0:
-    n1 = int(input("Digite o primeiro número: "))
-    n2 = int(input("Digite o segundo número: "))
+global n1
+global n2
+def __main__():
+    global n1, n2
+    n1=0
+    n2=0
+    while n1 <= 0 or n2 <= 0:
+        n1 = int(input("Digite o primeiro número: "))
+        n2 = int(input("Digite o segundo número: "))
+    diferenca()
 
 def diferenca():
+    global n1, n2
     dif = n1 - n2
 
     if dif < 0:
-        return "O segundo número ({}) é maior que o primeiro ({}).".format(n2, n1)
+        print("O segundo número ({}) é maior que o primeiro ({}).".format(n2, n1))
     elif dif > 0:
-        return "O primeiro número ({}) é maior que o segundo ({}).".format(n1, n2)
+        print("O primeiro número ({}) é maior que o segundo ({}).".format(n1, n2))
     else:
-        return "Os números são iguais."
+        print("Os números são iguais.")
     
-print(diferenca())
+if "__name__" == __main__():
+    diferenca()
