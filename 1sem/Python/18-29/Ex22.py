ac03d089cbc25e23d17a93c98e2a4b6250fc3be0
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-03-21 11:37:09 -0300

    Main 22

--- 1sem/Python/18-29/Ex22.py
+++ 1sem/Python/18-29/Ex22.py
@@ -1,15 +1,21 @@
-v1=0
-v2=0
-while v1 <= 0 or v2 <= 0:
-    v1 = int(input("Digite o primeiro valor: "))
-    v2 = int(input("Digite o segundo valor: "))
+global v1,v2
+def __main__():
+    global v1,v2
+    v1=0
+    2=0
+    while v1 <= 0 or v2 <= 0:
+        v1 = int(input("Digite o primeiro valor: "))
+        v2 = int(input("Digite o segundo valor: "))
+    ordem()
 
 def ordem():
+    global v1,v2
     if v1 > v2:
-        return f"A ordem crescente dos valores é: {v2}, {v1}"
+        print(f"A ordem crescente dos valores é: {v2}, {v1}")
     elif v2 > v1:
-        return f"A ordem crescente dos valores é: {v1}, {v2}"
+        print(f"A ordem crescente dos valores é: {v1}, {v2}")
     else:
-        return "Valores iguais"
-    
-print(ordem())
+        print("Valores iguais")
+
+if "__name__" == __main__():
+    ordem()
