6fb3890244fabac6390db6369b3ca059df0faf9c
Henrique S. Queiroz <henriquesq14@gmail.com>
2026-04-07 08:56:56 -0300

    Recursivas Ex1

---
+++ 1sem/Python/FunçõesRecursivas/Ex1.py
@@ -0,0 +1,13 @@
+# Soma de 1 a 100
+
+def somador(n):
+    while n < 100:
+        n=n+somador(n+1)
+    return n
+
+def main():
+    n=1
+    print(somador(n))
+
+if __name__ == '__main__':
+    main()
